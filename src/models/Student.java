9fcad019dd0be999994ef2688055c29be0f8f006
Jherald Kerby Catua <kerbycatua@gmail.com>
2025-07-22 21:57:46 +0800

    Student Module Page, Add Student, View All Student, Edit Student Info Addded

--- src/models/Student.java
+++ src/models/Student.java
@@ -6,7 +6,7 @@ public class Student {
     public String email;
     public int age;
 
-    Student(String fullName, String email, int age){
+    public Student(String fullName, String email, int age){
         this.fullName = fullName;
         this.email = email;
         this.age = age;
@@ -22,18 +22,6 @@ public class Student {
 
     public int getAge() {
         return age;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
 }
--- src/services/StudentService.java
+++ src/services/StudentService.java
@@ -2,6 +2,7 @@ package services;
 
 import java.sql.*;
 import java.util.*;
+import models.*;
 
 public class StudentService {
 
@@ -20,32 +21,124 @@ public class StudentService {
 
             String userChoice = scanner.nextLine();
             switch (userChoice) {
-                case "1" -> { // add new student
-
-                    System.out.print("Enter Student Full Name: ");
-                    String fullName = scanner.nextLine();
-
-                    System.out.print("Enter Student Email Name: ");
-                    String email = scanner.nextLine();
-
-                    System.out.print("Enter Student Age Name: ");
-                    String age = scanner.nextLine();
-
-                    try {
-                        PreparedStatement ps = con.prepareStatement("");
-                    } catch (SQLException e) {
-                        e.getStackTrace();
-                    }
-
-                }
-                case "2" -> System.out.println();
-                case "3" -> System.out.println();
+                case "1" -> addStudent(scanner, con);
+                case "2" -> viewAllStudent(con);
+                case "3" -> editStudentInfo(scanner, con);
                 case "4" -> System.out.println();
                 case "5" -> { // back
                     return;
                 }
                 default -> System.out.println("Invalid choice");
             }
+        }
+
+    }
+
+    public void addStudent(Scanner scanner, Connection con){
+
+        System.out.print("Enter Student Full Name: ");
+        String fullName = scanner.nextLine();
+
+        System.out.print("Enter Student Email Name: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Enter Student Age Name: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        Student student = new Student(fullName, email, age);
+
+        try {
+
+            PreparedStatement ps = con.prepareStatement("INSERT INTO Student(FullName, Email, Age) VALUES (?, ?, ?);");
+            ps.setString(1, student.getFullName());
+            ps.setString(2, student.getEmail());
+            ps.setInt(3, student.getAge());
+            ps.executeUpdate();
+            System.out.println("Student has been added successfully.");
+                        
+            } catch (SQLException e) {
+            e.getStackTrace();
+        }
+
+    }
+
+    public void viewAllStudent(Connection con){
+
+        try {
+            
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM Student;");
+            ResultSet rs = ps.executeQuery();
+
+            System.out.println();
+            while(rs.next()){
+                System.out.println(
+                    "Student ID [" + rs.getInt(1) + "]" + " | " +
+                    "Full Name: " + rs.getString(2) + " | " +
+                    "Email: " + rs.getString(3) + " | " +
+                    "Age: " + rs.getInt(4) + " | "
+                );
+            }
+
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+
+    }
+
+    public void editStudentInfo(Scanner scanner, Connection con){
+        
+        try {
+            
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM Student;");
+            ResultSet rs = ps.executeQuery();
+
+            System.out.println();
+            while(rs.next()){
+                System.out.println(
+                    "Student ID [" + rs.getInt(1) + "]" + " | " +
+                    "Full Name: " + rs.getString(2) + " | " +
+                    "Email: " + rs.getString(3) + " | " +
+                    "Age: " + rs.getInt(4) + " | "
+                );
+            }
+
+            System.out.print("\nSelect the student ID to edit: ");
+            int userInputIDToDelete = scanner.nextInt();
+            scanner.nextLine();
+
+            PreparedStatement ps1 = con.prepareStatement("SELECT FullName, Email, Age FROM Student WHERE StudentID = ?;");
+            ps1.setInt(1, userInputIDToDelete);
+            ResultSet rs1 = ps1.executeQuery();
+            while(rs1.next()){
+                System.out.println(
+                    "Full Name: " + rs1.getString(1) + " | " +
+                    "Email: " + rs1.getString(2) + " | " +
+                    "Age: " + rs1.getInt(3) + " | "
+                );
+            }
+
+            System.out.print("Enter new Full Name: ");
+            String newFullName = scanner.nextLine();
+
+            System.out.print("Enter new Email: ");
+            String newEmail = scanner.nextLine();
+
+            System.out.print("Enter new Age: ");
+            int newAge = scanner.nextInt();
+            scanner.nextLine();
+
+            Student student = new Student(newFullName, newEmail, newAge);
+            PreparedStatement ps2 = con.prepareStatement("UPDATE Student SET FullName = ?, Email = ?, Age = ? WHERE StudentID = ?;");
+            ps2.setString(1, student.getFullName());
+            ps2.setString(2, student.getEmail());
+            ps2.setInt(3, student.getAge());
+            ps2.setInt(4, userInputIDToDelete);
+            ps2.executeUpdate();
+            System.out.println("Student information has been successfully updated.");
+
+        } catch (SQLException e) {
+            e.getStackTrace();
         }
 
     }
