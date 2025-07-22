996bf58eb8933fddf66960cb21fe40d4d4340487
Jherald Kerby Catua <kerbycatua@gmail.com>
2025-07-22 15:14:33 +0800

    JDBC Added And DataBase is tested.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.sql.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        try {
+
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=StudentDB;integratedSecurity=true;encrypt=true;trustServerCertificate=true";
+            Connection con = DriverManager.getConnection(url);
+
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM Enrollment;");
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                System.out.println(
+                    rs.getInt("EnrollmentID") + " | " +
+                    rs.getInt("StudentID") + " | " +
+                    rs.getInt("CourseID") + " | " +
+                    rs.getDate("EnrollDate")    
+                    );
+            }
+
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+
     }
 }
