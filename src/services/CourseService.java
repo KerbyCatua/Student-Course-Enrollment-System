81efd295de9bcea20346f4de91423a0f3ddab9d4
Jherald Kerby Catua <kerbycatua@gmail.com>
2025-07-24 20:39:28 +0800

    Edit Course, Delete Course Added

--- src/services/CourseService.java
+++ src/services/CourseService.java
@@ -1,6 +1,7 @@
 package services;
 
 import java.util.Scanner;
+
 import java.sql.*;
 
 import models.*;
@@ -26,8 +27,8 @@ public class CourseService {
             switch (userChoice) {
                 case "1" -> addCourse(scanner, con);
                 case "2" -> viewAllCourses(con);
-                case "3" -> System.out.println(); // TODO edit course info
-                case "4" -> System.out.println(); // TODO delete course
+                case "3" -> editCourseInfo(scanner, con);
+                case "4" -> deleteCourse(scanner, con);
                 case "5" -> { // back
                     return;
                 }
@@ -71,6 +72,82 @@ public class CourseService {
                     "Course Name: " + rs.getString("CourseName") + " | "
                 );
             }
+
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+
+    }
+
+    public void editCourseInfo(Scanner scanner, Connection con){
+
+        try {
+            
+            System.out.println();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM Course;");
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                System.out.println(
+                    "Course ID [" + rs.getInt(1) + "] | " + 
+                    rs.getString(2) + " | "
+                );
+            }
+
+            System.out.print("\nEnter CourseID to Modify: ");
+            String courseIDToModify = scanner.nextLine();
+
+            PreparedStatement ps1 = con.prepareStatement("SELECT * FROM Course WHERE CourseID = ?;");
+            ps1.setInt(1, Integer.parseInt(courseIDToModify));
+            ResultSet rs1 = ps1.executeQuery();
+
+            while(rs1.next()){
+                System.out.println(
+                    rs1.getInt("CourseID") + " | " + rs1.getString("CourseName")
+                );
+            }
+
+            System.out.print("New Course Name: ");
+            String newCourseName = scanner.nextLine();
+            Course course = new Course(newCourseName);
+
+            PreparedStatement ps2 = con.prepareStatement("UPDATE Course SET CourseName = ? WHERE CourseID = ?;");
+            ps2.setString(1, course.getNewCourse());
+            ps2.setInt(2, Integer.parseInt(courseIDToModify));
+            ps2.executeUpdate();
+
+            System.out.println("Updated new Course Name.");
+            
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+
+    }
+
+    public void deleteCourse(Scanner scanner, Connection con){
+
+        try {
+
+            System.out.println();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM Course;");
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                System.out.println(
+                    "Course ID [" + rs.getInt(1) + "] | " + 
+                    rs.getString(2) + " | "
+                );
+            }
+            
+            System.out.print("\nEnter CourseID to Delete: ");
+            String courseIDToDelete = scanner.nextLine();
+            Course course = new Course(courseIDToDelete);
+
+            PreparedStatement ps1 = con.prepareStatement("DELETE FROM Course WHERE CourseID = ?;");
+            ps1.setInt(1, Integer.parseInt(course.getNewCourse()));
+            ps1.executeUpdate();
+            System.out.println("Course Deleted");
 
         } catch (SQLException e) {
             e.getStackTrace();
